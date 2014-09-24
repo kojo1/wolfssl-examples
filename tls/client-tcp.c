7b8ec3e4976a18e4b99d6d91955659a3af141d56
Takashi Kojo <kojo@wolfssl.com>
2014-09-24 11:11:22 +0900

    for embedded programs

--- tls/client-tcp.c
+++ tls/client-tcp.c
@@ -18,27 +18,67 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
+ 
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#include <cyassl/ctaocrypt/settings.h>
+
 #include    <stdio.h>
-#include    <unistd.h>
-#include    <stdlib.h>                  
+#include    <stdlib.h>
 #include    <string.h>
-#include    <errno.h>
-#include    <arpa/inet.h>
+
+#if defined(CYASSL_LWIP)
+     #include "lwip/sockets.h"
+     #define inet_pton(af, src, dst) ( *(unsigned long *)dst = inet_addr(src))
+#else
+     #include    <unistd.h>
+     #include    <errno.h>
+     #include    <arpa/inet.h>
+#endif
 
 #define MAXDATASIZE  4096   /* maximum acceptable amount of data */
 #define SERV_PORT    11111  /* define default port number */
 
+/* 
+ * resolving secape sequence
+ */
+static void esc_seq(char *buff) 
+{
+    int i, j ;
+
+    for(i=0, j=0; i<strlen(buff); i++, j++) {
+        if(buff[i]=='\\') {
+            switch (buff[i+1]) {
+            case 'n':
+                buff[j] = '\n' ;
+                break ;
+            case 'r':
+                buff[j] = '\r' ; 
+                break ;
+            default:  /* unknow cahr */
+                buff[j] = buff[i+1] ;
+                break ;
+            }
+            i++ ;
+        } else	buff[j] = buff[i] ;
+    }
+    buff[j+1] = '\0' ;
+}
+
 /*
  *  clients initial contact with server. Socket to connect to: sock
  */
-int ClientGreet(int sock)
+static int ClientGreet(int sock)
 {
     /* data to send to the server, data recieved from the server */
     char    sendBuff[MAXDATASIZE], rcvBuff[MAXDATASIZE] = {0};
-    int     err = 0;
+    int     ret, err = 0;
 
     printf("Message for server:\t");
     fgets(sendBuff, MAXDATASIZE, stdin);
+    esc_seq(sendBuff) ;
 
     if (write(sock, sendBuff, strlen(sendBuff)) != strlen(sendBuff)) {
         /* the message is not able to send, or error trying */
@@ -47,40 +87,52 @@ int ClientGreet(int sock)
         return EXIT_FAILURE;
     }
 
-    if (read(sock, rcvBuff, MAXDATASIZE) < 0) {
-        /* the server fails to send data, or error trying */
-        err = errno;
-        printf("Read error. errno: %i\n", err);
-        return EXIT_FAILURE;
-    }
-    printf("Recieved: \t%s\n", rcvBuff);
+    printf("Recieved: \t" ) ;
+    do {
+        ret = read(sock, rcvBuff, MAXDATASIZE) ;
+        if (ret < 0) {
+            /* the server fails to send data, or error trying */
+            err = errno;
+            printf("Read error. errno: %i\n", err);
+            return EXIT_FAILURE;
+        }
+        rcvBuff[ret] = '\0' ;
+        printf("%s", rcvBuff);
+    }while(ret > 0) ;
+		
     return 0;
 }
 /* 
  * command line argumentCount and argumentValues 
  */
-int main(int argc, char** argv) 
+#if defined(NO_MAIN_DRIVER)
+int client_tcp_main(int argc, char** argv) 
+#else
+int main(int argc, char** argv)
+#endif
+
 {
     int     sockfd;                         /* socket file descriptor */
     struct  sockaddr_in servAddr;           /* struct for server address */
     int     err;                            /* variable for error checks */
 
-    if (argc != 2) {
+    if ((argc != 2) && (argc != 3)) {
         /* if the number of arguments is not two, error */
-        printf("usage: ./client-tcp  <IP address>\n");
+        printf("usage: ./client-tcp  <IP address> [<Port number>]\n");
         return EXIT_FAILURE;
     }
 
     /* internet address family, stream based tcp, default protocol */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        printf("Failed to create socket. errono: %i\n", errno);
+        printf("Failed to create socket. errno: %i\n", errno);
         return EXIT_FAILURE;
     }
 
     memset(&servAddr, 0, sizeof(servAddr)); /* clears memory block for use */
     servAddr.sin_family = AF_INET;          /* sets addressfamily to internet*/
-    servAddr.sin_port = htons(SERV_PORT);   /* sets port to defined port */
+    servAddr.sin_port = (argc == 3) ? htons(atoi(argv[2])) : htons(SERV_PORT); 
+                                            /* sets port to defined port */
 
     /* looks for the server at the entered address (ip in the command line) */
     if (inet_pton(AF_INET, argv[1], &servAddr.sin_addr) < 1) {
@@ -96,5 +148,11 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
     ClientGreet(sockfd);
+
+#if defined(CYASSL_CLOSESOCKET)
+    closesocket(sockfd) ;
+#else
+    close(sockfd) ;
+#endif
     return 0;
 }
--- tls/client-tls.c
+++ tls/client-tls.c
@@ -18,29 +18,74 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
+ 
+ 
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#include "cyassl/ctaocrypt/settings.h"
+
 #include    <stdio.h>
-#include    <stdlib.h>                  
+#include    <stdlib.h>
 #include    <string.h>
-#include    <errno.h>
-#include    <arpa/inet.h>
+
+#if defined(CYASSL_LWIP)
+     #include "lwip/sockets.h"
+     #define inet_pton(af, src, dst) ( *(unsigned long *)dst = inet_addr(src))
+#else
+     #include    <unistd.h>
+     #include    <errno.h>
+     #include    <arpa/inet.h>
+#endif
+
 #include    <cyassl/ssl.h>          /* CyaSSL security library */
 
-#define MAXDATASIZE  4096           /* maximum acceptable amount of data */
-#define SERV_PORT    11111          /* define default port number */
+#define MAXDATASIZE  4096   /* maximum acceptable amount of data */
+#define SERV_PORT    11111    /* define default port number */
 
+#if !defined(NO_SERVER_CERT)
 const char* cert = "../certs/ca-cert.pem";
+#endif
+
+/* 
+ * resolving secape sequence
+ */
+static void esc_seq(char *buff) 
+{
+    int i, j ;
+
+    for(i=0, j=0; i<strlen(buff); i++, j++) {
+        if(buff[i]=='\\') {
+            switch (buff[i+1]) {
+            case 'n':
+                buff[j] = '\n' ;
+                break ;
+            case 'r':
+                buff[j] = '\r' ; 
+                break ;
+            default:  /* unknow cahr */
+                buff[j] = buff[i+1] ;
+                break ;
+            }
+            i++ ;
+        } else	buff[j] = buff[i] ;
+    }
+    buff[j+1] = '\0' ;
+}
 
 /* 
  * clients initial contact with server. (socket to connect, security layer)
  */
-int ClientGreet(int sock, CYASSL* ssl)
+static int ClientGreet(int sock, CYASSL* ssl)
 {
     /* data to send to the server, data recieved from the server */
-    char    sendBuff[MAXDATASIZE], rcvBuff[MAXDATASIZE] = {0};
+    char   sendBuff[MAXDATASIZE], rcvBuff[MAXDATASIZE] = {0};
     int     ret = 0;                /* variable for error checking */
 
     printf("Message for server:\t");
-    fgets(sendBuff, MAXDATASIZE, stdin);
+    fgets(sendBuff, MAXDATASIZE, stdin) ;
+    esc_seq(sendBuff) ;
 
     if (CyaSSL_write(ssl, sendBuff, strlen(sendBuff)) != strlen(sendBuff)) {
         /* the message is not able to send, or error trying */
@@ -49,39 +94,49 @@ int ClientGreet(int sock, CYASSL* ssl)
         return EXIT_FAILURE;
     }
 
-    if (CyaSSL_read(ssl, rcvBuff, MAXDATASIZE) < 0) {
-        /* the server failed to send data, or error trying */
-        ret = CyaSSL_get_error(ssl, 0);
-        printf("Read error. Error: %i\n", ret);
-        return EXIT_FAILURE;
-    }
-    printf("Recieved: \t%s\n", rcvBuff);
+    printf("Recieved: \t" ) ;
+    do {
+        ret = CyaSSL_read(ssl, rcvBuff, MAXDATASIZE-1) ;
+        if (ret < 0) {
+            /* the server failed to send data, or error trying */
+            ret = CyaSSL_get_error(ssl, 0);
+            printf("Read error. Error: %i\n", ret);
+            return EXIT_FAILURE;
+        }
+        rcvBuff[ret] = '\0' ;
+        printf("%s", rcvBuff);
+    }while(ret > 0) ;
 
-    return ret;
+    return 0;
 }
 
 /* 
  * applies TLS 1.2 security layer to data being sent.
  */
-int Security(int sock)
+static int Security(int sock)
 {
     CYASSL_CTX* ctx;
     CYASSL*     ssl;    /* create CYASSL object */
     int         ret = 0;
 
     CyaSSL_Init();      /* initialize CyaSSL */
-
+    /* CyaSSL_Debugging_ON() ;  */
+    
     /* create and initiLize CYASSL_CTX structure */
     if ((ctx = CyaSSL_CTX_new(CyaTLSv1_2_client_method())) == NULL) {
         printf("SSL_CTX_new error.\n");
         return EXIT_FAILURE;
     }
 
+#if !defined(NO_SERVER_CERT)
     /* load CA certificates into CyaSSL_CTX. which will verify the server */
     if (CyaSSL_CTX_load_verify_locations(ctx, cert, 0) != SSL_SUCCESS) {
         printf("Error loading %s. Please check the file.\n", cert);
         return EXIT_FAILURE;
     }
+#else
+    CyaSSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, 0);
+#endif
     if ((ssl = CyaSSL_new(ctx)) == NULL) {
         printf("CyaSSL_new error.\n");
         return EXIT_FAILURE;
@@ -91,6 +146,9 @@ int Security(int sock)
     ret = CyaSSL_connect(ssl);
     if (ret == SSL_SUCCESS) {
         ret = ClientGreet(sock, ssl);
+    } else {
+        printf("CyaSSL connection error.\n");
+        return EXIT_FAILURE;
     }
 
     /* frees all data before client termination */
@@ -104,15 +162,19 @@ int Security(int sock)
 /* 
  * Command line argumentCount and argumentValues 
  */
+#if defined(NO_MAIN_DRIVER)
+int client_tls_main(int argc, char** argv) 
+#else
 int main(int argc, char** argv) 
+#endif
 {
     int     sockfd;                         /* socket file descriptor */
     struct  sockaddr_in servAddr;           /* struct for server address */
     int     ret = 0;                        /* variable for error checking */
-
-    if (argc != 2) {
+	
+    if ((argc != 2) && (argc != 3)) {
         /* if the number of arguments is not two, error */
-        printf("usage: ./client-tcp  <IP address>\n");
+        printf("usage: ./client-tls  <IP address> [<Port number>]\n");
         return EXIT_FAILURE;
     }
 
@@ -126,7 +188,8 @@ int main(int argc, char** argv)
   
     memset(&servAddr, 0, sizeof(servAddr)); /* clears memory block for use */  
     servAddr.sin_family = AF_INET;          /* sets addressfamily to internet*/
-    servAddr.sin_port = htons(SERV_PORT);   /* sets port to defined port */
+    servAddr.sin_port = (argc == 3) ? htons(atoi(argv[2])) : htons(SERV_PORT);   
+                                            /* sets port to defined port */
 
     /* looks for the server at the entered address (ip in the command line) */
     if (inet_pton(AF_INET, argv[1], &servAddr.sin_addr) < 1) {
@@ -144,5 +207,10 @@ int main(int argc, char** argv)
     }
     Security(sockfd);
 
+#if defined(CYASSL_CLOSESOCKET)
+    closesocket(sockfd) ;
+#else
+    close(sockfd) ;
+#endif
     return ret;
 }
